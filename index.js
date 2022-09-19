c2d3f1a1d2b9ae5f7adfb309135261adbb79b02b
Brook Nega <brooknega21@gmail.com>
2022-09-18 23:01:37 -0400

    commit tree fd599181d426399397df5ceb648aca4b98336862

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
-// Write your code here
+// function multiply(num1, num2){
+//     // let num1 = 31
+//     // let num2 = 2
+//     return (num1 * num2)
+//     // 31 * 2
+// }
+
+let num1 = 31
+let num2 = 2
+
+let multiply = num1 * num2
+
+let random = Math.floor((Math.random() * 10))
+
+let num3 = 64
+let num4 = 12
+
+let mod = num3 % num4
+
+let max = Math.max(2,12,20,3)
